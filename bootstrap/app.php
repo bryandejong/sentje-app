ed64a5b994e960c328b2430665322f185e063c6f
bryandejong <bdjong1@avans.nl>
2019-04-02 14:45:38 +0200

    Date localisation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Propaganistas\LaravelIntl\IntlServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
