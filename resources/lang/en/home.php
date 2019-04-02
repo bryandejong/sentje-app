b30394d709db27872395b058f1350159182c7761
mussaf <38488831+mussaf@users.noreply.github.com>
2019-04-02 17:27:24 +0200

    translations Account, Home and Contacts

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -3,5 +3,16 @@
 return [
 
     'overview' => 'Overview',
-
+    'intro' => 'Welcome to this beautiful admin panel.',
+    'firstname' => 'Firstname',
+    'lastname' => 'Lastname',
+    'age' => 'Age',
+    'bankaccounts' => 'Bank accounts',
+    'delete' => 'Delete',
+    'banksafe' => 'We\'ll never share your IBAN with anyone else.',
+    'enterBank' => 'Enter Bank',
+    'addBank' => 'Adding a bank',
+    'contact' => 'Connections',
+    'addContact' => 'Add a connection',
+    'enterEmail' => 'Enter email'
 ];
--- resources/lang/nl/home.php
+++ resources/lang/nl/home.php
@@ -3,5 +3,16 @@
 return [
 
     'overview' => 'Overzicht',
-
+    'intro'    => 'Welkom bij onze mooie dashboard.',
+    'firstname' => 'Voornaam',
+    'lastname' => 'Achternaam',
+    'age' => 'leeftijd',
+    'bankaccounts' => 'Bankrekeningen',
+    'delete' => 'Verwijderen',
+    'banksafe' => 'Uw bank account is veilig bij ons.',
+    'enterBank' => 'Uw Bank',
+    'addBank' => 'Bank aanmaken',
+    'contact' => 'Contacten',
+    'addContact' => 'Contact toevoegen',
+    'enterEmail' => 'Uw email'
 ];
