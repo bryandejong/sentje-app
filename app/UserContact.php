1ef5e4ac00cd650f45e2b942fa66575221ae32e9
mussaf <38488831+mussaf@users.noreply.github.com>
2019-05-13 13:25:21 +0200

    PSR2

--- app/UserContact.php
+++ app/UserContact.php
@@ -1,16 +1,17 @@
 <?php
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UserContact extends Model
 {
-    public function user(){
+    public function user()
+    {
         return $this->hasOne('App\User', 'id', 'user_id');
     }
 
-    public function contact() {
+    public function contact()
+    {
         return $this->hasOne('App\User', 'id', 'contact_id');
     }
 }
